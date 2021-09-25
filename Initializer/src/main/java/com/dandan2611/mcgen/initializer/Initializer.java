3617eda9a41d10194e12dafabac922cf8273dab2
Adam CAVILLON <dandan2611@gameinbox.net>
2021-09-25 22:43:00 +0200

    Created initializer main

---
+++ Initializer/src/main/java/com/dandan2611/mcgen/initializer/Initializer.java
@@ -0,0 +1,13 @@
+package com.dandan2611.mcgen.initializer;
+
+public class Initializer {
+
+    /**
+     * Init the application
+     * @param args Launch params
+     */
+    public void init(String[] args) {
+
+    }
+
+}
---
+++ Initializer/src/main/java/com/dandan2611/mcgen/initializer/InitializerMain.java
@@ -0,0 +1,9 @@
+package com.dandan2611.mcgen.initializer;
+
+public class InitializerMain {
+
+    public static void main(String[] args) {
+        new Initializer().init(args);
+    }
+
+}
