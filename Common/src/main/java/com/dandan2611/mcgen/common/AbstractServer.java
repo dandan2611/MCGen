cba4a8c5b60f8a312291d53a6c3ef7ae3582e771
Adam CAVILLON <dandan2611@gameinbox.net>
2021-09-25 22:51:04 +0200

    Created AbstractServer init and exit methods

--- Common/src/main/java/com/dandan2611/mcgen/common/AbstractServer.java
+++ Common/src/main/java/com/dandan2611/mcgen/common/AbstractServer.java
@@ -3,6 +3,16 @@ package com.dandan2611.mcgen.common;
 public abstract class AbstractServer {
 
     /**
+     * Initialize the game server
+     */
+    public abstract void init();
+
+    /**
+     * Exit and delete the game server files
+     */
+    public abstract void exit();
+
+    /**
      * Start the game server
      */
     public abstract void start();
