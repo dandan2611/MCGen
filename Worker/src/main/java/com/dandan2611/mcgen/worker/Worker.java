2c58863d7093c39de840eb344912dcaa23971931
Adam CAVILLON <dandan2611@gameinbox.net>
2021-09-25 22:52:45 +0200

    Created worker main class

---
+++ Worker/src/main/java/com/dandan2611/mcgen/worker/Worker.java
@@ -0,0 +1,13 @@
+package com.dandan2611.mcgen.worker;
+
+public class Worker {
+
+    /**
+     * Init worker
+     * @param args Startup parameters
+     */
+    public void init(String[] args) {
+
+    }
+
+}
---
+++ Worker/src/main/java/com/dandan2611/mcgen/worker/WorkerMain.java
@@ -0,0 +1,9 @@
+package com.dandan2611.mcgen.worker;
+
+public class WorkerMain {
+
+    public static void main(String[] args) {
+        new Worker().init(args);
+    }
+
+}
