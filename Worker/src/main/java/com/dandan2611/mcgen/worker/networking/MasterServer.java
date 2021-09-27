44f1574a34cda2c5c837410f04fd0547dd8802e3
Adam CAVILLON <dandadounet@gmail.com>
2021-09-27 15:36:52 +0200

    Added Handlers registration into MasterServer

--- Worker/src/main/java/com/dandan2611/mcgen/worker/networking/MasterServer.java
+++ Worker/src/main/java/com/dandan2611/mcgen/worker/networking/MasterServer.java
@@ -1,10 +1,13 @@
 package com.dandan2611.mcgen.worker.networking;
 
+import com.dandan2611.mcgen.worker.networking.handlers.ChannelRegisterHandler;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,7 +37,14 @@ public class MasterServer {
         serverBootstrap.group(bossGroup, workerGroup)
                 .channel(NioServerSocketChannel.class)
                 .option(ChannelOption.SO_BACKLOG, 128)
-                .option(ChannelOption.SO_KEEPALIVE, true);
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        socketChannel.pipeline()
+                                .addLast(new ChannelRegisterHandler());
+                    }
+                });
 
         this.channelFuture = serverBootstrap.bind(port);
 
