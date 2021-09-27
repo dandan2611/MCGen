a4120eafe21273aa3888feac2d9a216755a50097
Adam CAVILLON <dandadounet@gmail.com>
2021-09-27 16:50:27 +0200

    Created EncodedPacket class

--- Common/src/main/java/com/dandan2611/mcgen/common/networking/EncodedPacket.java
+++ Common/src/main/java/com/dandan2611/mcgen/common/networking/EncodedPacket.java
@@ -1,2 +1,18 @@
-package com.dandan2611.mcgen.common.networking;public class EncodedPacket {
+package com.dandan2611.mcgen.common.networking;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
+public class EncodedPacket {
+
+    private ByteBuf buf;
+
+    public EncodedPacket() {
+        this.buf = Unpooled.buffer();
+    }
+
+    private ByteBuf getBuffer() {
+        return this.buf;
+    }
+
 }
