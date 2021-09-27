5db886192e4cdfbf926aa2c89d0df647e48def57
Adam CAVILLON <dandadounet@gmail.com>
2021-09-27 16:03:22 +0200

    Deleted unused import

--- Worker/src/main/java/com/dandan2611/mcgen/worker/networking/WorkerClient.java
+++ Worker/src/main/java/com/dandan2611/mcgen/worker/networking/WorkerClient.java
@@ -5,7 +5,6 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
-import io.netty.channel.epoll.EpollEventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
