ecfa6e5465665389f53b9781bfa7a1c51ad9e51b
Adam CAVILLON <dandadounet@gmail.com>
2021-09-27 10:11:59 +0200

    Created MasterServer class

--- Worker/src/main/java/com/dandan2611/mcgen/worker/networking/MasterServer.java
+++ Worker/src/main/java/com/dandan2611/mcgen/worker/networking/MasterServer.java
@@ -1,2 +1,28 @@
-package com.dandan2611.mcgen.worker.networking;public class MasterServer {
+package com.dandan2611.mcgen.worker.networking;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MasterServer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MasterServer.class);
+
+    private int port;
+
+    public MasterServer(int port) {
+        this.port = port;
+    }
+
+    public void init() {
+        LOGGER.info("Initializing master server");
+
+        LOGGER.info("Master server initialized!");
+    }
+
+    public void exit() {
+        LOGGER.info("Exiting master server");
+
+        LOGGER.info("Master server exited!");
+    }
+
 }
