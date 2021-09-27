d855dd8fc50edaef79ce6558f804e7f9d27737ff
Adam CAVILLON <dandadounet@gmail.com>
2021-09-27 10:20:21 +0200

    Created MasterServer default methods

--- Worker/src/main/java/com/dandan2611/mcgen/worker/networking/MasterServer.java
+++ Worker/src/main/java/com/dandan2611/mcgen/worker/networking/MasterServer.java
@@ -1,5 +1,11 @@
 package com.dandan2611.mcgen.worker.networking;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,6 +15,11 @@ public class MasterServer {
 
     private int port;
 
+    private EventLoopGroup bossGroup, workerGroup;
+    private ChannelFuture channelFuture;
+
+    private Thread syncThread;
+
     public MasterServer(int port) {
         this.port = port;
     }
@@ -16,11 +27,41 @@ public class MasterServer {
     public void init() {
         LOGGER.info("Initializing master server");
 
+        bossGroup = new NioEventLoopGroup();
+        workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, 128)
+                .option(ChannelOption.SO_KEEPALIVE, true);
+
+        this.channelFuture = serverBootstrap.bind(port);
+
+        this.syncThread = new Thread(() -> {
+            try {
+                channelFuture.sync();
+            }
+            catch (InterruptedException exception) {
+                LOGGER.info("Master ChannelFuture sync() interrupted");
+            }
+        }, "masterserver_sync");
+        syncThread.start();
+
         LOGGER.info("Master server initialized!");
     }
 
     public void exit() {
         LOGGER.info("Exiting master server");
+
+        try {
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        workerGroup.shutdownGracefully();
+        bossGroup.shutdownGracefully();
 
         LOGGER.info("Master server exited!");
     }
