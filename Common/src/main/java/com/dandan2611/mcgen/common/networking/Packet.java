dbcd67c55edb2378ffeabf26502e1a60eaddb3af
Adam CAVILLON <dandadounet@gmail.com>
2021-09-27 16:57:22 +0200

    Deleted EncodedPacket

--- Common/src/main/java/com/dandan2611/mcgen/common/networking/Packet.java
+++ Common/src/main/java/com/dandan2611/mcgen/common/networking/Packet.java
@@ -1,2 +1,7 @@
-package com.dandan2611.mcgen.common.networking;public class Packet {
+package com.dandan2611.mcgen.common.networking;
+
+import java.io.Serializable;
+
+public abstract class Packet implements Serializable {
+
 }
