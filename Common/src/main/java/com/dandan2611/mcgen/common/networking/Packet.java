34e0a54c4b02b1d1906b6ae6c1c01f0de9de98ee
Adam CAVILLON <dandan2611@gameinbox.net>
2021-09-28 17:09:41 +0200

    Added Packet id, encode and decode methods

--- Common/src/main/java/com/dandan2611/mcgen/common/networking/Packet.java
+++ Common/src/main/java/com/dandan2611/mcgen/common/networking/Packet.java
@@ -1,7 +1,13 @@
 package com.dandan2611.mcgen.common.networking;
 
-import java.io.Serializable;
 
-public abstract class Packet implements Serializable {
+import com.google.gson.JsonObject;
+
+public abstract class Packet<T extends Packet<?>> {
+
+    public abstract int id();
+
+    public abstract EncodedPacket encode();
+    public abstract T decode(JsonObject data);
 
 }
