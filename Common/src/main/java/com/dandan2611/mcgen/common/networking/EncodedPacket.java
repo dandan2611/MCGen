d689e153775aee7a37609d632ba8a8c004da4004
Adam CAVILLON <dandan2611@gameinbox.net>
2021-09-28 17:09:15 +0200

    Added EncodedPacket Packet id

--- Common/src/main/java/com/dandan2611/mcgen/common/networking/EncodedPacket.java
+++ Common/src/main/java/com/dandan2611/mcgen/common/networking/EncodedPacket.java
@@ -1,27 +1,30 @@
 package com.dandan2611.mcgen.common.networking;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 
 public class EncodedPacket {
 
+    private int packetId;
     private ByteBuf buffer;
 
-    public EncodedPacket() {
+    public EncodedPacket(int packetId) {
+        this.packetId = packetId;
         this.buffer = Unpooled.buffer();
-    }
-
-    public EncodedPacket(ByteBuf buffer) {
-        this.buffer = buffer;
     }
 
     public void setData(JsonObject element) {
         if(buffer.readableBytes() > 0)
             throw new UnsupportedOperationException("Data already set");
-        String json = new Gson().toJson(element);
+
+        JsonObject object = new JsonObject();
+
+        object.addProperty("packetId", packetId);
+        object.add("data", element);
+
+        String json = new Gson().toJson(object);
         buffer.writeBytes(json.getBytes());
     }
 
